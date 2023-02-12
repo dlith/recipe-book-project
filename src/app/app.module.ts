76f530eef9ddeae6e920722cbfe9f126bf381d99
Dzmitry Liashuk <dlyashuk1997@gmail.com>
2023-02-12 10:34:53 +0100

    Providing the Recipe Service Correctly

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,7 @@ import { ShoppingListService } from './shopping-list/shopping-list.service';
 import { AppRoutingModule } from './app-routing.service';
 import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
 import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
+import { RecipeService } from './recipes/recipe.service';
 
 @NgModule({
     declarations: [
@@ -30,7 +31,7 @@ import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component
       RecipeStartComponent,
       RecipeEditComponent
     ],
-    providers: [ShoppingListService],
+    providers: [ShoppingListService, RecipeService],
     bootstrap: [AppComponent],
     imports: [
       BrowserModule,
