4f80aafe13f51606b87d8e6108b1716b342de783
Dzmitry Liashuk <dlyashuk1997@gmail.com>
2023-02-23 18:44:25 +0100

    Setting Up the DataStorage Service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,6 +16,7 @@ import { AppRoutingModule } from './app-routing.service';
 import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
 import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 import { RecipeService } from './recipes/recipe.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
     declarations: [
@@ -37,7 +38,8 @@ import { RecipeService } from './recipes/recipe.service';
       BrowserModule,
       FormsModule,
       AppRoutingModule,
-      ReactiveFormsModule
+      ReactiveFormsModule,
+      HttpClientModule
     ]
 })
 export class AppModule { }
