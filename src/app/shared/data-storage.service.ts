1d04581cd96eb58eceac7050db4ae86a95f25d81
Dzmitry Liashuk <dlyashuk1997@gmail.com>
2023-02-23 18:53:09 +0100

    Storing Recipes

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,12 +1,21 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { RecipeService } from "../recipes/recipe.service";
 
 @Injectable({
     providedIn: 'root'
 })
 export class DataStorageService {
     
-    constructor(private http: HttpClient) {
+    private readonly BASE_URL = 'https://ng-course-recipe-book-ebcff-default-rtdb.europe-west1.firebasedatabase.app/recipes.json';
 
+    constructor(private http: HttpClient, private recipeService: RecipeService) {}
+
+    storeRecipes(){
+        const recipes = this.recipeService.getRecipes();
+        this.http.put(this.BASE_URL, recipes)
+        .subscribe(response=>{
+            console.log(response);
+        });
     }
 }
