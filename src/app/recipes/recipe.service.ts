d21564ef08414266c69183954b2f07ca8244ace5
Dzmitry Liashuk <dlyashuk1997@gmail.com>
2023-02-24 17:34:05 +0100

    Transforming Response Data

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -9,24 +9,24 @@ export class RecipeService {
 
     recipesChanged = new Subject<Recipe[]>;
 
-    private recipes: Recipe[]  = [
-        new Recipe('Tasty Schnitzel', 
-            'A super-tasty Schnitzel - just awesome!', 
-            'https://insanelygoodrecipes.com/wp-content/uploads/2022/03/Homemade-Pork-Schnitzel-with-Cauliflower-and-Lemons.jpg',
-            [new Ingredient('Meat', 1), new Ingredient('Grench Fries', 20)]
-            ),
-        new Recipe('Big Fat Burger', 
-            'What else you need to say?', 
-            'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTEBXg9Y0Q8bLvmuTmuveq7ie92J6cw25UPgQ&usqp=CAU',
-            [new Ingredient('Buns', 2), new Ingredient('Meat', 20)]
-            ),
-        new Recipe('Greece Salad ', 
-            'This is the best salad!', 
-            'https://www.mygreekdish.com/wp-content/uploads/2013/05/Traditional-Greek-Salad-recipe-Horiatiki-Xoriatiki-1-scaled.jpg',
-            [new Ingredient('Salad', 2), new Ingredient('Feta', 20)]
-            )
-      ];
-
+    // private recipes: Recipe[]  = [
+    //     new Recipe('Tasty Schnitzel', 
+    //         'A super-tasty Schnitzel - just awesome!', 
+    //         'https://insanelygoodrecipes.com/wp-content/uploads/2022/03/Homemade-Pork-Schnitzel-with-Cauliflower-and-Lemons.jpg',
+    //         [new Ingredient('Meat', 1), new Ingredient('Grench Fries', 20)]
+    //         ),
+    //     new Recipe('Big Fat Burger', 
+    //         'What else you need to say?', 
+    //         'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTEBXg9Y0Q8bLvmuTmuveq7ie92J6cw25UPgQ&usqp=CAU',
+    //         [new Ingredient('Buns', 2), new Ingredient('Meat', 20)]
+    //         ),
+    //     new Recipe('Greece Salad ', 
+    //         'This is the best salad!', 
+    //         'https://www.mygreekdish.com/wp-content/uploads/2013/05/Traditional-Greek-Salad-recipe-Horiatiki-Xoriatiki-1-scaled.jpg',
+    //         [new Ingredient('Salad', 2), new Ingredient('Feta', 20)]
+    //         )
+    //   ];
+    private recipes: Recipe[] = [];
       
     constructor(private shoppinngListService: ShoppingListService){}
 
--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { map } from "rxjs/operators";
 import { Recipe } from "../recipes/recipe.model";
 import { RecipeService } from "../recipes/recipe.service";
 
@@ -21,7 +22,13 @@ export class DataStorageService {
     }
 
     fetchRecipes() {
-        this.http.get<Recipe[]>(this.BASE_URL).subscribe(recipes => {
+        this.http.get<Recipe[]>(this.BASE_URL)
+        .pipe(map(recipes=>{
+            return recipes.map(recipe=> {
+                return {...recipe, ingredients: recipe.ingredients ? recipe.ingredients : []};
+            });
+        }))
+        .subscribe(recipes => {
             this.recipeService.setRecipes(recipes);
         });
     }
