4822c5183c49aa520d46c72f7ff2ced029d76a40
Dzmitry Liashuk <dlyashuk1997@gmail.com>
2023-02-27 18:29:09 +0100

    Login Error Handling

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpErrorResponse } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { catchError, throwError } from "rxjs";
 
@@ -27,17 +27,7 @@ export class AuthService {
           email: email,
           password: password,
           returnSecureToken: true
-        }).pipe(catchError(errorRes => {
-          let errorMessage = 'An unknown erro occurred!';
-          if (!errorRes.error || !errorRes.error.error) {
-            return throwError(() => new Error(errorMessage));
-          }
-          switch(errorRes.error.error.message) {
-            case 'EMAIL_EXISTS': 
-              errorMessage = 'This email exists already!';
-          } 
-          return throwError(() => new Error(errorMessage));
-        }));
+        }).pipe(catchError(this.handleError));
     }
 
     login(email: string, password: string) {
@@ -47,6 +37,25 @@ export class AuthService {
           password: password,
           returnSecureToken: true
         }
-      );
+      ).pipe(catchError(this.handleError));
+    }
+
+    private handleError(errorRes: HttpErrorResponse) {
+      let errorMessage = 'An unknown error occurred!';
+          if (!errorRes.error || !errorRes.error.error) {
+            return throwError(() => new Error(errorMessage));
+          }
+          switch(errorRes.error.error.message) {
+            case 'EMAIL_EXISTS': 
+              errorMessage = 'This email exists already!';
+              break;
+            case 'EMAIL_NOT_FOUND':
+              errorMessage = 'This email does not found!';
+              break;
+            case 'INVALID_PASSWORD':
+              errorMessage = 'This password is not correct!';
+              break;
+          } 
+          return throwError(() => new Error(errorMessage));
     }
 }
