13714521b2204d4133e348affd2517caca889ceb
Dzmitry Liashuk <dlyashuk1997@gmail.com>
2023-03-01 16:00:23 +0100

    Attaching the Token with an Interceptor

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -15,23 +15,23 @@ export class DataStorageService {
     constructor(private http: HttpClient, private recipeService: RecipeService, private authService: AuthService) {}
 
     storeRecipes(){
-        const recipes = this.recipeService.getRecipes();
-        this.http.put(this.BASE_URL, recipes)
-        .subscribe(response=>{
-            console.log(response);
-        });
+      const recipes = this.recipeService.getRecipes();
+      this.http.put(this.BASE_URL, recipes)
+      .subscribe(response=>{
+        console.log(response);
+      });
     }
 
     fetchRecipes() {
-        return this.authService.user.pipe(take(1), exhaustMap(user => {
-            return this.http.get<Recipe[]>(this.BASE_URL, {params: new HttpParams().set('auth', user.token)});
-        }), map(recipes=>{
-            return recipes.map(recipe=> {
-                return {...recipe, ingredients: recipe.ingredients ? recipe.ingredients : []};
-            });
+      return this.http.get<Recipe[]>(this.BASE_URL).pipe(
+        map(recipes=>{
+          return recipes.map(recipe=> {
+            return {...recipe, ingredients: recipe.ingredients ? recipe.ingredients : []};
+          });
         }), 
         tap(recipes=> {
-            this.recipeService.setRecipes(recipes);
-        }));
+          this.recipeService.setRecipes(recipes);
+        })
+      );
     }
 }
