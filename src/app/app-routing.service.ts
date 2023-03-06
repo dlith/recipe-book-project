a9e3dd40d01f9a9b057e36c0459f42a5e63b7a06
Dzmitry Liashuk <dlyashuk1997@gmail.com>
2023-03-06 16:16:02 +0100

    Adding Auth Feature Module

--- src/app/app-routing.service.ts
+++ src/app/app-routing.service.ts
@@ -1,17 +1,12 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
-import { AuthComponent } from "./auth/auth.component";
-import { ShoppingListComponent } from "./shopping-list/shopping-list.component";
 
 const appRoutes: Routes = [
-    { path: '', redirectTo: '/recipes', pathMatch: 'full'},
-    { path: 'auth', component: AuthComponent }
+    { path: '', redirectTo: '/recipes', pathMatch: 'full'}
 ];
 
 @NgModule({
     imports: [RouterModule.forRoot(appRoutes)],
     exports: [RouterModule]
 })
-export class AppRoutingModule {
-
-}
+export class AppRoutingModule {}
