2ec6b75d71fa97b89f4ff0ee7143c25ae03d1ed7
Dzmitry Liashuk <dlyashuk1997@gmail.com>
2023-03-22 16:39:46 +0100

    Expanding the State

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -5,6 +5,7 @@ import { Subscription } from 'rxjs';
 import { Ingredient } from 'src/app/shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list.service';
 import * as ShoppingListActions from '../store/shopping-list.actions';
+import * as fromShoppingList from '../store/shopping-list.reducer';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -19,7 +20,7 @@ export class ShoppingEditComponent implements OnInit, OnDestroy{
   editedItem: Ingredient;
   @ViewChild('f', {static: false}) slForm: NgForm;
 
-  constructor(private shoppingListService: ShoppingListService, private store: Store<{shoppingList: { ingredients: Ingredient[] }}>){}
+  constructor(private shoppingListService: ShoppingListService, private store: Store<fromShoppingList.AppState>){}
 
   ngOnInit(){
     this.subscription = this.shoppingListService.startedEditing.subscribe(
