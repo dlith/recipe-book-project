2cee6fd7cdf8780e59ecb5dda1abb315a8c68ea0
Dzmitry Liashuk <dlyashuk1997@gmail.com>
2023-03-25 10:35:59 +0100

    One Root State

--- src/app/shopping-list/store/shopping-list.reducer.ts
+++ src/app/shopping-list/store/shopping-list.reducer.ts
@@ -8,11 +8,6 @@ export interface State {
   editedIngredientIndex: number
 }
 
-export interface AppState {
-  shoppingList: State;
-}
-
-
 const initialState: State = {
   ingredients: [
     new Ingredient('Apples', 5),
