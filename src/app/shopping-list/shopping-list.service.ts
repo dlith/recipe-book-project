3d190d70acf5d1ca6166fb776846860f756b98f8
Dzmitry Liashuk <dlyashuk1997@gmail.com>
2023-03-25 10:23:05 +0100

    clean up shopping list service

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,46 +1,46 @@
-import { EventEmitter } from "@angular/core";
-import { Subject } from "rxjs";
-import { Ingredient } from "../shared/ingredient.model";
+// import { EventEmitter } from "@angular/core";
+// import { Subject } from "rxjs";
+// import { Ingredient } from "../shared/ingredient.model";
 
-export class ShoppingListService {
+// export class ShoppingListService {
     
-    ingredientsChanged = new Subject<Ingredient[]>();
-    startedEditing = new Subject<number>();
+//     ingredientsChanged = new Subject<Ingredient[]>();
+//     startedEditing = new Subject<number>();
 
-    private ingredients: Ingredient[] = [
-        new Ingredient('Apples', 5),
-        new Ingredient('Tomatoes', 10)
-    ];
+//     private ingredients: Ingredient[] = [
+//         new Ingredient('Apples', 5),
+//         new Ingredient('Tomatoes', 10)
+//     ];
 
 
-    addIngredient(ingregient: Ingredient){
-        this.ingredients.push(ingregient);
-        this.ingredientsChanged.next(this.ingredients.slice());
-    }
+//     addIngredient(ingregient: Ingredient){
+//         this.ingredients.push(ingregient);
+//         this.ingredientsChanged.next(this.ingredients.slice());
+//     }
 
-    getIngredient(index: number) {
-        return this.ingredients[index];
-    }
+//     getIngredient(index: number) {
+//         return this.ingredients[index];
+//     }
 
-    getIngredients(){
-        return this.ingredients.slice();
-    }
+//     getIngredients(){
+//         return this.ingredients.slice();
+//     }
 
-    addIngredients(ingredients: Ingredient[]){
-        // for(let ingredient of ingredients) {
-        //     this.addIngredient(ingredient);
-        // }
-        this.ingredients.push(...ingredients);
-        this.ingredientsChanged.next(this.ingredients.slice());
-    }
+//     addIngredients(ingredients: Ingredient[]){
+//         // for(let ingredient of ingredients) {
+//         //     this.addIngredient(ingredient);
+//         // }
+//         this.ingredients.push(...ingredients);
+//         this.ingredientsChanged.next(this.ingredients.slice());
+//     }
 
-    updateIngredient(index: number, newIngredient: Ingredient) {
-        this.ingredients[index] = newIngredient;
-        this.ingredientsChanged.next(this.ingredients.slice());
-    }
+//     updateIngredient(index: number, newIngredient: Ingredient) {
+//         this.ingredients[index] = newIngredient;
+//         this.ingredientsChanged.next(this.ingredients.slice());
+//     }
 
-    deleteIngregient(index: number){
-        this.ingredients.splice(index, 1);
-        this.ingredientsChanged.next(this.ingredients.slice());
-    }
-}
+//     deleteIngregient(index: number){
+//         this.ingredients.splice(index, 1);
+//         this.ingredientsChanged.next(this.ingredients.slice());
+//     }
+// }
